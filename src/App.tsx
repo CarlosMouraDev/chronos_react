5029c6557dd2d10f195dbbb15c953074d53e159e
Carlos Eduardo <cadumouralemes@gmail.com>
2025-07-18 16:28:48 -0300

    refactor: improving pagination file system

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,9 @@
 import './styles/theme.css';
 import './styles/global.css';
-import { Container } from './components/Container';
-import { Logo } from './components/Logo';
-import { Menu } from './components/Menu';
-import { CountDown } from './components/CountDown';
-import { MainForm } from './components/MainForm';
-import { Footer } from './components/Footer';
+import Home from './pages/Home';
 
 function App() {
-  return (
-    <>
-      <Container>
-        <Logo />
-      </Container>
-
-      <Container>
-        <Menu />
-      </Container>
-
-      <Container>
-        <CountDown />
-      </Container>
-
-      <Container>
-        <MainForm />
-      </Container>
-
-      <Container>
-        <Footer />
-      </Container>
-    </>
-  );
+  return <Home />;
 }
 
 export default App;
