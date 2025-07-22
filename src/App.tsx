3a47b6fbb00bb4bb3364d5fd0b8a5b4ece3a2285
Carlos Eduardo <cadumouralemes@gmail.com>
2025-07-22 15:43:16 -0300

    feat: creating TaskContext

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,6 @@
 import './styles/theme.css';
 import './styles/global.css';
 import Home from './pages/Home';
-import type { TaskStateModel } from './models/TaskStateModel';
-
-const initialState: TaskStateModel = {
-  tasks: [],
-  secondsRemaining: 0,
-  formatedSecondsRemaining: '00:00',
-  activeTask: null,
-  currentCycle: 0,
-  config: {
-    workTime: 25,
-    shortBreakTime: 5,
-    longBreakTime: 15,
-  },
-};
 
 function App() {
   return <Home />;
