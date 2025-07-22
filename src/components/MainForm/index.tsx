24aac890409b93e50948adb606b88493ca3c2d4a
Carlos Eduardo <cadumouralemes@gmail.com>
2025-07-22 17:49:28 -0300

    feat: initial logic for form MainForm input

--- src/components/MainForm/index.tsx
+++ src/components/MainForm/index.tsx
@@ -3,16 +3,59 @@ import { DefaultButton } from '../DefaultButton';
 import styles from './styles.module.css';
 import { Cycles } from '../Cycles';
 import { DefaultInput } from '../DefaultInput';
+import { useRef } from 'react';
+import type { TaskModel } from '../../models/TaskModel';
+import { useTaskContext } from '../../hooks/useTaskContext';
 
 export function MainForm() {
+  const { setState } = useTaskContext();
+  const taskNameInput = useRef<HTMLInputElement>(null);
+
+  function handleSubmit(event: React.FormEvent<HTMLFormElement>) {
+    event.preventDefault();
+
+    if (taskNameInput.current === null) return;
+
+    const taskName = taskNameInput.current.value.trim();
+
+    if (!taskName) {
+      alert('divite o nome da tarefa');
+    }
+
+    const newTask: TaskModel = {
+      id: Date.now().toString(),
+      name: taskName,
+      startDate: Date.now(),
+      completeDate: null,
+      interruptDate: null,
+      duration: 1,
+      type: 'workTime',
+    };
+
+    const secondsRemaining = newTask.duration * 60;
+
+    setState(prevState => {
+      return {
+        ...prevState,
+        config: { ...prevState.config },
+        activeTask: newTask,
+        currentCycle: 1,
+        secondsRemaining,
+        formattedSecondsRemaining: '00:00',
+        tasks: [...prevState.tasks, newTask],
+      };
+    });
+  }
+
   return (
-    <form className={styles.form} action=''>
+    <form onSubmit={handleSubmit} className={styles.form} action=''>
       <div className={styles.formRow}>
         <DefaultInput
-          id='selecione'
+          id='time'
           type='text'
           labelText='teste'
           placeholder='digite algo'
+          ref={taskNameInput}
         />
       </div>
       <div className={styles.formRow}>
