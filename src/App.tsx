6424ed1e3e8d62d550719103c93f36298d96629c
Carlos Eduardo <cadumouralemes@gmail.com>
2025-07-21 22:45:54 -0300

    feat: adding initialState config

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,20 @@
 import './styles/theme.css';
 import './styles/global.css';
-import NotFound from './pages/NotFound';
 import Home from './pages/Home';
-import { AboutPomodoro } from './pages/AboutPomodoro';
+import type { TaskStateModel } from './models/TaskStateModel';
+
+const initialState: TaskStateModel = {
+  tasks: [],
+  secondsRemaining: 0,
+  formatedSecondsRemaining: '00:00',
+  activeTask: null,
+  currentCycle: 0,
+  config: {
+    workTime: 25,
+    shortBreakTime: 5,
+    longBreakTime: 15,
+  },
+};
 
 function App() {
   return <Home />;
